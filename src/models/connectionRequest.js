6ec8f5d1292c28ae7e65179d3972ea1c8b10648a
Adeel Haider <adeelhaider3360@gmail.com>
2026-03-02 02:15:19 +0500

    compoundIndex

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -23,11 +23,13 @@ const connectionRequestSchema= new mongoose.Schema(
 
     },{timestamps:true}
 )
+        //compound index (indexes make that query very fast)
+    connectionRequestSchema.index({fromUserId:1,toUserId:1})  //1 means ascending order, -1 means descending order. we can use any one of them as it is just for searching purpose and it will not affect the result
+    //e.g 
+    //anydatabasecollection.index({name:1},{gpa:-1})                       
 
-    //schema level validation to ensure that only one connection request can be sent from one user to another
+    //why should we not use index on every field? because it will take more space and it will also slow down the write operations as it has to update the index every time we insert or update a document. so we should use index only on those fields which are frequently used in search queries.
 
-    //connectionRequestSchema.index({fromUserId:1,toUserId:1},{unique:true})  //to ensure that only one connection request can be sent from one user to another
-    
     //schema level validation to ensure that a user cannot send connection request to himself
 
     connectionRequestSchema.pre('save', function(next) {  //pre check before saving the document (e.g here connection.save())
