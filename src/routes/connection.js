1c07f9fac4f36b4e910506ffcdc62bb82401d1be
Adeel Haider <adeelhaider3360@gmail.com>
2026-03-02 01:31:22 +0500

    connectionEdgeCases

--- src/routes/connection.js
+++ src/routes/connection.js
@@ -12,33 +12,49 @@ router.post('/sendConnection/:status/:toUserId',userAuth,async(req,res)=>{
     try {
         
     const fromUser=req.user
-    const toUser=await findOne(req.params.toUserId); //find the user to whom connection request is to be sent
-    const status=req.params.status;                 //interested,ignored,accepted,rejected
+    
+    const toUser=await User.findById(req.params.toUserId);   //find the user to whom connection request is to be sent
+    if(!toUser){
+        return res.status(404).send({message:"User not found"})
+    }
+    const status=req.params.status;                          //interested,ignored
+       
+    if(fromUser._id.equals(toUser._id)){
+            return res.status(400).send({message:"You cannot send connection request to yourself"})
+        }
 
-    if(status=='accepted'){
-    //create condition if connection request already exists between two users
-    const existingConnectionRequest=await ConnectionRequest.findOne({
-        from:fromUser._id,
-        to:toUser._id
+    const allowedStatus=["interested","ignored"]
+    if(!allowedStatus.includes(status)){
+        return res.status(400).send({message:"Invalid status type"})
+    }
+
+    const checkConnection=await ConnectionRequest.findOne({
+        $or: [  //used to check multiple conditions to see if it satisfy any one of them
+            {fromUserId:fromUser._id,toUserId:toUser._id},  //from user to other
+            {fromUserId:toUser._id,toUserId:fromUser._id}   //from other to User
+        ]
     })
 
-        if(existingConnectionRequest){
-            return res.send({message:"Connection request already exists"})
+    if(!checkConnection){
+        return res.status(400).send({message:"Connection already exist"})
         }
-    }
+
     //create a connection request
     const connectionRequest=new ConnectionRequest({
-        from:fromUser._id,
-        to:toUser._id,
+        fromUserId:fromUser._id,
+        toUserId:toUser._id,
         status:status
     })
+
+    
+      await connectionRequest.save();
+    res.send({message:"Connection request sent successfully",connectionRequest})
 
     } catch (error) {
         res.send({message:error.message})
     }
     
-    await connectionRequest.save();
-    res.send({message:"Connection request sent successfully",connectionRequest})
+    
     
 })
 
